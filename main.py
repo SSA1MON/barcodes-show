8e6ecef714d267b4c1f3516ec70351bef2897a03
SSA1MON <zagorulko.kirill@gmail.com>
2024-04-09 00:17:54 +0300

    barcode generation without saving a file

--- main.py
+++ main.py
@@ -1,63 +1,56 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-from io import BytesIO
+from PIL import Image, ImageTk
 from barcode import Code39
 from barcode.writer import ImageWriter
-from io import BytesIO
 
 
-MY_CODE = Code39
+# TODO: rework
+BARCODE_TYPE = Code39
 
 
-def generate_barcode():
+def generate_barcode(event=None):
     """
-    Временное описание:
-    Берет строку из поля ввода, генерирует штрих-код, сохраняет его как файл,
-    изменяет label на данные из строки, обновляет переменную с изображением,
-    удаляет текст из поля ввода и фокусирует курсор на нем.
+    Генерирует штрих-код на основе введенного текста и обновляет отображение.
+    Args:
+        event: Костыль для срабатывания функции при нажатии <Enter>
     """
-    global python_logo
-    input_barcode = barcode_area.get()
-    my_barcode = MY_CODE(input_barcode, writer=ImageWriter())
-    my_barcode.save(f'img/{input_barcode}_barcode')
+    input_text = barcode_area.get()
+    my_barcode = BARCODE_TYPE(input_text, writer=ImageWriter())
+    img = my_barcode.render()
+    img = ImageTk.PhotoImage(img)
 
-    fp = BytesIO()
-    my_barcode.write(fp)
+    label.config(text=input_text)
+    label.image = img
+    label.config(image=label.image)
 
-    with open(f"img/{input_barcode}_barcode.png", "wb") as f:
-        my_barcode.write(f)
-
-    label["text"] = input_barcode
-
-    barcode_img = PhotoImage(file=f"img/{input_barcode}_barcode.png", width=650)
-    label.image = barcode_img
-    label['image'] = label.image
-
-    barcode_area.delete(0, END)
+    barcode_area.delete(0, tk.END)
     barcode_area.focus()
 
 
-window = Tk()
+window = tk.Tk()
+window.iconbitmap(default="favicon.ico")
 window.title('Barcodes show')
 window.geometry('650x500')
+window.minsize(650, 500)
 
-python_logo = PhotoImage(file="img/start_barcode.png")
-label = ttk.Label(image=python_logo, text="YOUR BARCODE IS HERE", compound="top", font=('Arial Black', 32))
+default_image = Image.open("img/start_barcode.png")
+default_image = ImageTk.PhotoImage(default_image)
+label = ttk.Label(
+    image=default_image, text="BARCODE NUMBER",
+    compound="top", font=('Arial Black', 32)
+)
 label.pack()
 
-frame = Frame(master=window, relief=SUNKEN, borderwidth=2)
+frame = tk.Frame(master=window)
 frame.pack(expand=True)
 
-barcode_area = Entry(frame, width=90)
+barcode_area = tk.Entry(frame, width=30)
 barcode_area.grid(row=1, column=2)
+barcode_area.bind("<Return>", generate_barcode)
 barcode_area.focus()
 
-
-cal_btn = ttk.Button(frame, text='Generate barcode', command=generate_barcode)
+cal_btn = ttk.Button(frame, text='Generate barcode', command=generate_barcode, padding=7, width=28)
 cal_btn.grid(row=2, column=2, pady=10)
 
-label2 = ttk.Label(frame, text="213312321", compound="top")
-label2.grid(row=3, column=2)
-
-if __name__ == '__main__':
-    window.mainloop()
+window.mainloop()
