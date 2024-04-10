8d5442f747130f94a2c396c6ea9dd72561d9f3a1
SSA1MON <zagorulko.kirill@gmail.com>
2024-04-10 11:19:45 +0300

    minor changes

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 import tkinter as tk
-from tkinter import ttk
-import sys
+from tkinter import ttk, S
 from PIL import Image, ImageTk
 from barcode import EAN13, ITF
 from barcode.writer import ImageWriter
+import webbrowser
+import sys
+
+
+def open_github_url(event):
+    webbrowser.open_new(r"https://github.com/SSA1MON/barcodes-show")
 
 
 def about_help_menu():
     about_window = tk.Toplevel(padx=10, pady=10)
     about_window.focus()
-    about_window.title("About Barcodes show")
+    about_window.title("About")
     about_window.geometry("400x250")
     about_window.minsize(400, 250)
     about_window.maxsize(400, 250)
 
-    about_label1 = ttk.Label(about_window, text="alpha-build", font=('Verdana', 12))
-    about_label2 = ttk.Label(about_window, text="Github")
-    about_label1.pack()
-    about_label2.pack()
+    title_label = ttk.Label(about_window, text="Barcodes show", font=('Verdana', 14))
+    url_label = ttk.Label(about_window, text="Github", foreground='#0000EE', cursor="hand2")
+    version_label = ttk.Label(about_window, text="alpha-build")
+    title_label.pack()
+    version_label.pack()
+    url_label.pack(expand=True, anchor=S)
+    url_label.bind("<Button-1>", open_github_url)
 
 
 def exit_window_menu():
@@ -33,7 +41,7 @@ def identify_barcode_type(input_text, barcode_type=None):
     return barcode_type
 
 
-def generate_barcode(event=None):
+def generate_barcode(event):
     """
     Генерирует штрих-код на основе введенного текста и обновляет отображение.
     Args:
@@ -49,17 +57,24 @@ def generate_barcode(event=None):
             img = my_barcode.render()
         img = ImageTk.PhotoImage(img)
 
-        label.config(text='')
-        label_barcode_info.config(text=str(barcode_type))
+        error_label.pack_forget()
+        label_barcode_info.pack()
+        label_barcode_info.config(text="Тип: " + str(barcode_type))
         start_barcode.image = img
         start_barcode.config(image=start_barcode.image)
 
         barcode_area.delete(0, tk.END)
         barcode_area.focus()
     else:
-        label.config(text="Неподдерживаемый формат", font=("Arial Black", 26))
+        error_label.pack()
+        error_label.config(
+            text="Неподдерживаемый формат",
+            font=("Verdana", 12), background='#d9534f', foreground='white'
+        )
         barcode_area.delete(0, tk.END)
         barcode_area.focus()
+        label_barcode_info.pack_forget()
+        start_barcode.config(image=default_image)
 
 
 window = tk.Tk()
@@ -87,20 +102,20 @@ start_barcode = ttk.Label(
 )
 start_barcode.pack()
 
-label = ttk.Label(font=('Arial Black', 32), background='white')
-label.pack()
+error_label = ttk.Label(background='white')
+error_label.pack()
 
 frame = tk.Frame(master=window, background='white')
 frame.pack(expand=True)
 
-barcode_area = tk.Entry(frame, width=30)
+barcode_area = tk.Entry(frame, width=39)
 barcode_area.grid(row=1, column=2)
 barcode_area.bind("<Return>", generate_barcode)
 barcode_area.focus()
 
-gen_btn_style = ttk.Style().configure('TButton', background='white')
+gen_btn_style = ttk.Style().configure('TButton', background='white', font=('Arial', 11))
 cal_btn = ttk.Button(
-    frame, text='Сгенерировать штрих-код',
+    frame, text='Сгенерировать',
     command=generate_barcode, padding=7, width=28, style='TButton'
 )
 cal_btn.grid(row=2, column=2, pady=10)
